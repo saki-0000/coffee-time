3837e4d33a3cb915f158d1f5f9dd90636a7c3ee1
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-01-16 13:28:01 +0900

    router

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/coffee-time/',
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
