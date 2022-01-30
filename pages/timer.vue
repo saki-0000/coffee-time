cbf5de88ccb66e915f4f41973abf0e8ef2c24d36
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-01-30 18:26:15 +0900

    yarn lintfix

--- pages/timer.vue
+++ pages/timer.vue
@@ -3,8 +3,8 @@
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
         <v-text-field
-          type="number"
           v-model.number="second"
+          type="number"
           label="秒"
           hint="何秒はかりますか？？"
         />
@@ -18,8 +18,8 @@
 import { defineComponent } from '@vue/composition-api'
 import Timer from '../components/Timer.vue'
 export default defineComponent({
-  components: { Timer },
   name: 'TimerPage',
+  components: { Timer },
   setup: () => {
     const second: Number = 180
     return {
