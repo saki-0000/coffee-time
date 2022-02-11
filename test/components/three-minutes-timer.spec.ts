c5fe7f790052e92431bdf07ea183262c2a7def0c
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-02-11 17:02:20 +0900

    formatTimeのテストが雑だがいったん完了とします。

--- test/components/three-minutes-timer.spec.ts
+++ test/components/three-minutes-timer.spec.ts
@@ -1,11 +1,22 @@
-import { shallowMount, Wrapper } from '@vue/test-utils'
+import { mount, Wrapper } from '@vue/test-utils'
 import ThreeMinutesTimer from '~/components/ThreeMinutesTimer.vue'
 import * as composable from '~/composable/timer'
+
+let measureTime = jest.fn(() => {})
+let time = jest.fn(() => 100)
+let formatTime = jest.fn(() => '100')
+jest.mock('~/composable/timer', () => ({
+  useTimer: () => ({
+    measureTime: measureTime,
+    time: time,
+    formatTime: formatTime,
+  }),
+}))
 
 describe('ThreeMinutesTimer.vue', () => {
   let wrapper: Wrapper<any>
   beforeEach(() => {
-    wrapper = shallowMount(ThreeMinutesTimer)
+    wrapper = mount(ThreeMinutesTimer)
   })
 
   test('renders correctly', () => {
@@ -13,10 +24,13 @@ describe('ThreeMinutesTimer.vue', () => {
   })
   test('call useTime`s functions when the `start` button clicked ', () => {
     wrapper.get('#startButton').trigger('click')
-    expect(wrapper.get('#displayTime').text()).toBe('03:00')
+    expect(measureTime).toHaveBeenCalledTimes(1)
+    expect(formatTime).toHaveBeenCalledTimes(2)
+    expect(wrapper.get('#displayTime').text()).toBe('100')
   })
-  test('v-btn to be disabled when the `start` button clicked ', () => {
+  test('v-btn to be disabled when the `start` button clicked ', async () => {
     wrapper.get('#startButton').trigger('click')
-    expect(wrapper.get('#displayTime').text()).toBe('03:00')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.get('#startButton').attributes().disabled).toBe('disabled')
   })
 })
