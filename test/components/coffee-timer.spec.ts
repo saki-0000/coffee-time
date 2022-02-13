aa1ebc8e6419c59a08adacafab03232f843935d0
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-02-13 13:10:58 +0900

    コーヒータイマーのスナップショットテストを追加

--- test/components/coffee-timer.spec.ts
+++ test/components/coffee-timer.spec.ts
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, Wrapper } from '@vue/test-utils'
 import CoffeeTimer from '~/components/CoffeeTimer.vue'
 import ThreeMinutesTimer from '~/components/ThreeMinutesTimer.vue'
 
@@ -9,8 +9,14 @@ jest.mock('~/composable/coffee-timer', () => ({
 }))
 
 describe('CoffeeTimer', () => {
+  let wrapper: Wrapper<any>
+  beforeEach(() => {
+    wrapper = shallowMount(CoffeeTimer)
+  })
+  test('renders correctly', () => {
+    expect(wrapper.html()).toMatchSnapshot()
+  })
   test('子コンポーネントからchangeイベントを受けたときにその返り値が表示されていること', () => {
-    const wrapper = shallowMount(CoffeeTimer)
     wrapper.findComponent(ThreeMinutesTimer).vm.$emit('change', 0)
     expect(wrapper.text()).toBe('test')
   })
